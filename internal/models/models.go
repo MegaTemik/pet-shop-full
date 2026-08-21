c7b997faa83f81980f03f66786a92d4c8e8f0ffa
MegaTemik <artemlisovin2007@gmail.com>
2026-08-21 19:22:22 +0400

    fix: models + products

--- internal/models/models.go
+++ internal/models/models.go
@@ -17,7 +17,8 @@ type User struct {
 
 type Order struct {
 	ID         int       `json:"id"`
-	CustomerID int       `json:"customer_id"`
+	UserEmail  string    `json:"user_email"`
+	TotalPrice float64   `json:"total_price"`
 	CreatedAt  time.Time `json:"created_at"`
 }
 
